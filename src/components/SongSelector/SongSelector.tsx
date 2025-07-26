7d42be1cabc4973279a3e033a4b799f763e4c3f1
fMuchka <f.muchka@gmail.com>
2025-07-26 23:24:17 +0200

    F!! apply theme to song groups in selector

--- src/components/SongSelector/SongSelector.tsx
+++ src/components/SongSelector/SongSelector.tsx
@@ -1,6 +1,21 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, darken, lighten, styled, TextField } from "@mui/material";
 import type { RefinedSong } from "../../utils/rawSongRefiner";
 import type { SongSelectorProps } from "./types";
+
+const GroupHeader = styled('div')(({ theme }) => ({
+  position: 'sticky',
+  top: '-8px',
+  padding: '4px 10px',
+  color: theme.palette.primary.main,
+  backgroundColor: lighten(theme.palette.primary.light, 0.85),
+  ...theme.applyStyles('dark', {
+    backgroundColor: darken(theme.palette.primary.main, 0.8),
+  }),
+}));
+
+const GroupItems = styled('ul')({
+  padding: 0,
+});
 
 const SongSelector = (props: SongSelectorProps) => {
     const { songs, selectedSong, setSelectedSong } = props;
@@ -18,7 +33,7 @@ const SongSelector = (props: SongSelectorProps) => {
         <Autocomplete
             color="primary"
             value={{ label: selectedSong?.id, value: selectedSong?.id }}
-            disablePortal
+            
             options={songsList(songs)}
             groupBy={(option) => {
                 if (option.label === undefined) {
@@ -28,6 +43,12 @@ const SongSelector = (props: SongSelectorProps) => {
                 return option.label.split(' - ')[0]
             }}
             renderInput={(params) => <TextField {...params} label="Song" />}
+            renderGroup={(params) => (
+                <li key={params.key}>
+                    <GroupHeader>{params.group}</GroupHeader>
+                    <GroupItems>{params.children}</GroupItems>
+                </li>
+            )}
             onChange={handleSongSelectorChange}
       />
     )
